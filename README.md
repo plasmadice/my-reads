690b989099b9ccbf7d6b11b07c0575fac9c5cbdf
Herman White <phoenixquint@gmail.com>
2018-10-01 01:21:48 -0400

    Added todos, and fixed back button search page

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+ISSUES:
+Search Page TODOs;
+Books not being able to manipulating the server yet, research update method;
+
+
 # MyReads Project
 
 This is the starter template for the final assessment project for Udacity's React Fundamentals course. The goal of this template is to save you time by providing a static example of the CSS and HTML markup that may be used, but without any of the React code that is needed to complete the project. If you choose to start with this template, your job will be to add interactivity to the app by refactoring the static code in this template.
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import * as BooksAPI from '../BooksAPI';
 import Book from './Book';
+import { Link } from 'react-router-dom';
 
 class Search extends Component {
     state = {
@@ -8,6 +9,8 @@ class Search extends Component {
         books: [],
     }
 
+    // TODO: address issue with 'art' vs 'artificial intellegence'
+    // TODO: might be related, search for multiple words
     fetchBooks = (query) => {
         BooksAPI.search(query).then(books => {
             this.setState({ books: books })
@@ -24,13 +27,12 @@ class Search extends Component {
         e.preventDefault();
         this.setState({ query: e.target.value })
     }
-
     render() {
         
         return (
             <div className="search-books">
                 <div className="search-books-bar">
-                <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+                <Link to='/' className="close-search">Close</Link>
                     <div className="search-books-input-wrapper">
                         {/*
                         NOTES: The search from BooksAPI is limited to a particular set of search terms.
