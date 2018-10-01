3f68f1eb7278b5eb068abc2035c0ab639c894fc2
Herman White <phoenixquint@gmail.com>
2018-09-30 20:15:45 -0400

    Book generator component implemented

---
+++ src/components/Book.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+class Books extends Component {
+  render() {
+    const { book } = this.props;
+    
+    return (
+        <div className="book">
+            <div className="book-top">
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                <div className="book-shelf-changer">
+                <select>
+                    <option value="move" disabled>Move to...</option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">None</option>
+                </select>
+                </div>
+            </div>
+            <div className="book-title">{book.title}</div>
+            <div className="book-authors">{book.authors.map((author, index) => {
+                // Handles multiple authors
+                return (book.authors.length !== index+1) ? `${author}, ` : author;
+            })}</div>
+        </div>
+    )
+  }
+}
+
+export default Books;
