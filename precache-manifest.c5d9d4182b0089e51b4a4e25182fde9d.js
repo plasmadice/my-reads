201a64766322a8f80e3f2141e0ca423d2d815d1a
Herman White <phoenixquint@gmail.com>
2019-01-30 00:35:24 -0500

    Updates

--- precache-manifest.c5d9d4182b0089e51b4a4e25182fde9d.js
+++ precache-manifest.c5d9d4182b0089e51b4a4e25182fde9d.js
@@ -20,12 +20,12 @@ self.__precacheManifest = [
     "url": "/my-reads/static/media/add.bb9363b8.svg"
   },
   {
-    "revision": "3687aa988bec28f17ae39153db1646c3",
-    "url": "/my-reads/static/media/arrow-back.3687aa98.svg"
-  },
-  {
     "revision": "78c6f53b2342643639fe1cbaf171bf47",
     "url": "/my-reads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/my-reads/static/media/arrow-back.3687aa98.svg"
   },
   {
     "revision": "ed0ccf5688b4556b7f63c423c7d15560",
